d8021ae765308efdc62ebf78135fb0087310be14
Vivek Parekh <vp@vivekparekh.ca>
2019-04-13 16:23:05 -0400

    Fixed eslint errors

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="Vivek Parekh Portfolio" keywords={[`Vivek`, `Parekh`, `portfilio`, `resume`]} />
+        <h1>This site is still under construction!</h1>
+    </Layout>
+);
+
+export default IndexPage;
